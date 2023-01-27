e87b2bb3d81780e6ced1e8e4bfa955400a676ae4
bib12ag <bib12ag@naver.com>
2023-01-27 18:38:42 +0900

    Add data loader

--- LeNet-5.py
+++ LeNet-5.py
@@ -5,9 +5,22 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from torchvision import datasets, transforms 
+from torchvision.transforms import Compose, Resize, ToTensor
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 
 ''' gpu '''
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Current device is', DEVICE)
+
+''' data loader '''
+transform = Compose([Resize((32, 32)), ToTensor()])
+
+train_dataset = datasets.MNIST(root='.', train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root='.', train=False, transform=transform, download=True)
+
+BATCH_SIZE = 128
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
